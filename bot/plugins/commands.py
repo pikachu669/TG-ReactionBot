27e14211f2e848bb6419d9e762fc5a7272f9139f
pikachu669 <132365833+pikachu669@users.noreply.github.com>
2024-03-29 11:49:31 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -11,21 +11,9 @@ from bot.modules.static import *
         filters.group
     )
 )
-async def start_command(_, msg: Message):
-    return await msg.reply(
-        text=WelcomeText % {'first_name': msg.from_user.first_name if msg.from_user else 'Anonymous'},
-        quote=True,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(text='Add me to chat', url=f'https://t.me/{Telegram.BOT_USERNAME}?startgroup=botstart')
-                ],
-                [
-                    InlineKeyboardButton(text='Source Code', url='https://github.com/TheCaduceus/TG-ReactionBot')
-                ]
-            ]
-        )
-    )
+
+        
+    
 
 @TelegramBot.on_message(
     filters.command('emojis')
@@ -41,7 +29,7 @@ async def send_emojis(_, msg: Message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(text='Reference', url='https://github.com/TheCaduceus/TG-ReactionBot/blob/main/bot/config.py#L8')
+                    InlineKeyboardButton(text='Reference', url='t.me')
                 ]
             ]
         )
